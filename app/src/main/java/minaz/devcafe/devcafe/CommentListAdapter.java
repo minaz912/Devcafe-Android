db7ad0e2ff0b1bc13f0a8af9c8463d81114da2c5
Mina S. Aziz <minaz922@gmail.com>
2015-07-02 20:44:09 +0200

    Post Comments to an Idea and auto-update comments

--- app/src/main/java/minaz/devcafe/devcafe/CommentListAdapter.java
+++ app/src/main/java/minaz/devcafe/devcafe/CommentListAdapter.java
@@ -1,11 +1,13 @@
 package minaz.devcafe.devcafe;
 
 import android.app.Activity;
+import android.content.Context;
 import android.database.DataSetObserver;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 import android.widget.ImageView;
-import android.widget.ListAdapter;
 import android.widget.TextView;
 
 import com.squareup.picasso.Picasso;
@@ -15,14 +17,17 @@ import java.util.List;
 /**
  * Created by minaz on 30/06/15.
  */
-public class CommentListAdapter implements ListAdapter {
+public class CommentListAdapter extends ArrayAdapter<Comment> {
 
-    public Activity activity;
+    private final Context context;
     public List<Comment> comments;
+    private int layoutResourceId;
 
-    public CommentListAdapter(Activity activity, List<Comment> comments) {
-        this.activity = activity;
+    public CommentListAdapter(Context context, int layoutResourceId, List<Comment> comments) {
+        super(context, layoutResourceId, comments);
+        this.context = context;
         this.comments = comments;
+        this.layoutResourceId = layoutResourceId;
     }
 
     @Override
@@ -51,7 +56,7 @@ public class CommentListAdapter implements ListAdapter {
     }
 
     @Override
-    public Object getItem(int position) {
+    public Comment getItem(int position) {
         return comments.get(position);
     }
 
@@ -67,28 +72,39 @@ public class CommentListAdapter implements ListAdapter {
 
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
-        Comment comment = (Comment) getItem(position);
-        if (convertView == null) {
-            convertView = activity.getLayoutInflater().inflate(R.layout.list_item_comment, null);
+        View row = convertView;
+        ViewHolder holder = null;
+
+        if (row == null) {
+            LayoutInflater inflater = ((Activity) context).getLayoutInflater();
+            row = inflater.inflate(layoutResourceId, parent, false);
+
+            holder = new ViewHolder();
+            holder.commentTimestamp = (TextView) row.findViewById(R.id.comment_timestamp);
+            holder.commentOwner = (TextView) row.findViewById(R.id.comment_owner);
+            holder.commentOwnerPic = (ImageView) row.findViewById(R.id.comment_owner_pic);
+            holder.commentText = (TextView) row.findViewById(R.id.comment_text);
+            row.setTag(holder);
+        } else {
+            holder = (ViewHolder) row.getTag();
         }
-        TextView commentTimestamp = (TextView) convertView.findViewById(R.id.comment_timestamp);
-        TextView commentOwner = (TextView) convertView.findViewById(R.id.comment_owner);
-        ImageView commentOwnerPic = (ImageView) convertView.findViewById(R.id.comment_owner_pic);
-        TextView commentText = (TextView) convertView.findViewById(R.id.comment_text);
 
-//        Time time = comment.timestamp.getTime();
-//        SimpleDateFormat simpleDateFormat = new SimpleDateFormat(comment.timestamp);
-//        String dateTime = simpleDateFormat.format("yyyy-MM-dd HH:mm:ssZ");
+        Comment comment = comments.get(position);
 
-//        commentTimestamp.setText(String.valueOf(dateTime));
-        commentTimestamp.setText(formatDateTime(comment.timestamp));
-        commentOwner.setText("by: " + comment.owner.username);
-        commentText.setText(comment.text);
+        holder.commentTimestamp.setText(formatDateTime(comment.timestamp));
+        holder.commentOwner.setText("by: " + comment.owner.username);
+        holder.commentText.setText(comment.text);
         if (comment.owner.picture != null) {
-            Picasso.with(activity).load(comment.owner.picture).into(commentOwnerPic);
+            Picasso.with(this.context).load(comment.owner.picture).into(holder.commentOwnerPic);
         }
 
-        return convertView;
+        return row;
+    }
+
+    @Override
+    public void notifyDataSetChanged() {
+        super.notifyDataSetChanged();
+
     }
 
     public String formatDateTime(String dt) {
@@ -109,5 +125,12 @@ public class CommentListAdapter implements ListAdapter {
     @Override
     public boolean isEmpty() {
         return comments.size() == 0;
+    }
+
+    static class ViewHolder {
+        TextView commentTimestamp;
+        TextView commentOwner;
+        ImageView commentOwnerPic;
+        TextView commentText;
     }
 }
--- app/src/main/java/minaz/devcafe/devcafe/IdeaDetailActivity.java
+++ app/src/main/java/minaz/devcafe/devcafe/IdeaDetailActivity.java
@@ -5,15 +5,20 @@ import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 
 import java.io.BufferedReader;
+import java.io.DataOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
@@ -24,8 +29,15 @@ import java.util.Arrays;
 
 public class IdeaDetailActivity extends AppCompatActivity {
 
+    private int mIdeaID;
+    private TextView postCommentTextView;
+    private Button postCommentButton;
+    private ArrayList<Comment> commentList;
+    private CommentListAdapter adapter;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_idea_detail);
 
@@ -46,10 +58,30 @@ public class IdeaDetailActivity extends AppCompatActivity {
         String[] params = new String[1];
         params[0] = uri.toString();
         task.execute(params);
+
+        postCommentTextView = (TextView) findViewById(R.id.idea_detail_postComment_textview);
+        postCommentButton = (Button) findViewById(R.id.idea_detail_postComment_button);
+
+        postCommentButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String commentText = postCommentTextView.getText().toString();
+                if (!commentText.isEmpty()) {
+//                    String[] params = new String[1];
+//                    params[0] = commentText;
+                    PostCommentTask postCommentTask = new PostCommentTask();
+                    postCommentTask.execute(commentText);
+                }
+            }
+        });
     }
 
     public Idea parseIdeaDetail(String response) {
         return new Gson().fromJson(response, Idea.class);
+    }
+
+    public Comment parsePostedComment(String response) {
+        return new Gson().fromJson(response, Comment.class);
     }
 
     @Override
@@ -122,13 +154,111 @@ public class IdeaDetailActivity extends AppCompatActivity {
             TextView ideaTitle = (TextView) findViewById(R.id.idea_detail_ideaTitle_textview);
             TextView ideaDescription = (TextView) findViewById(R.id.idea_detail_ideaDescription_textview);
             TextView ideaOwner = (TextView) findViewById(R.id.idea_detail_ideaOwner_textview);
+            mIdeaID = result.id;
             ideaTitle.setText(result.title);
             ideaDescription.setText(result.description);
             ideaOwner.setText("by: " + result.owner.username);
             final ListView ideasListView = (ListView) findViewById(R.id.commentListView);
-            ArrayList<Comment> commentList = new ArrayList<>(Arrays.asList(result.comments));
-            CommentListAdapter adapter = new CommentListAdapter(IdeaDetailActivity.this, commentList);
+
+            commentList = new ArrayList<>(Arrays.asList(result.comments));
+            adapter = new CommentListAdapter(IdeaDetailActivity.this,
+                    R.layout.list_item_comment, commentList);
             ideasListView.setAdapter(adapter);
+            progressDialog.dismiss();
+            super.onPostExecute(result);
+        }
+    }
+
+    private class PostCommentTask extends AsyncTask<String, Void, Comment> {
+
+        ProgressDialog progressDialog = new ProgressDialog(IdeaDetailActivity.this);
+
+        @Override
+        protected Comment doInBackground(String... params) {
+
+            // params comes from the execute() call: params[0] is the url.
+            try {
+                Uri addCommentUri = Uri.parse(MainActivity.BASE_URL).buildUpon()
+                        .appendPath("ideas")
+                        .appendPath(String.valueOf(mIdeaID))
+                        .appendPath("add_comment/")
+                        .build();
+                URL addCommentURL = new URL(addCommentUri.toString());
+                HttpURLConnection connection = (HttpURLConnection) addCommentURL.openConnection();
+                connection.setRequestMethod("POST");
+
+                String authHeader = "Token " + MainActivity.AUTH_TOKEN.replaceAll("\"", "");
+                Log.d("TOKEN", authHeader);
+
+                connection.setRequestProperty("Authorization", authHeader);
+                connection.setRequestProperty("Content-Type", "application/json");
+                connection.setRequestProperty("Accept", "application/json");
+                connection.setDoOutput(true);
+                connection.connect();
+
+                Comment comment = new Comment();
+                comment.text = params[0];
+                Gson gson = new GsonBuilder().excludeFieldsWithoutExposeAnnotation().create();
+                String request = gson.toJson(comment);
+                DataOutputStream dataOutputStream = new DataOutputStream(connection
+                        .getOutputStream());
+                dataOutputStream.writeBytes(request);
+                dataOutputStream.flush();
+                dataOutputStream.close();
+
+                Log.v("Comment RESPONSE CODE", String.valueOf(connection.getResponseCode()));
+
+                if (connection.getResponseCode() != 201) {
+                    InputStream stream = connection.getErrorStream();
+                    BufferedReader reader = new BufferedReader(new InputStreamReader(stream));
+                    StringBuilder sb = new StringBuilder();
+                    String line;
+                    while ((line = reader.readLine()) != null) {
+                        sb.append(line).append("\n");
+                    }
+                    reader.close();
+                    Log.d("POST COMMENT RESPONSE", sb.toString());
+                    return parsePostedComment(sb.toString());
+                } else {
+                    InputStream stream = connection.getInputStream();
+                    BufferedReader reader = new BufferedReader(new InputStreamReader(stream));
+                    StringBuilder sb = new StringBuilder();
+                    String line;
+                    while ((line = reader.readLine()) != null) {
+                        sb.append(line).append("\n");
+                    }
+                    reader.close();
+                    Log.d("POST COMMENT RESPONSE", sb.toString());
+                    return parsePostedComment(sb.toString());
+                }
+
+            } catch (IOException e) {
+                e.printStackTrace();
+//                return "Unable to retrieve web page. URL may be invalid.";
+                return null;
+            }
+        }
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            try {
+                progressDialog.setMessage("Loading...");
+                progressDialog.show();
+//                progressDialog.show(MainActivity.this, "Loading...", "Please Wait...");
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+
+        // onPostExecute displays the results of the AsyncTask.
+        @Override
+        protected void onPostExecute(Comment result) {
+            commentList.add(result);
+            adapter.notifyDataSetChanged();
+            //TODO: comment owner pic not set when posting a new comment
+            postCommentTextView.setText("");
+            postCommentTextView.clearFocus();
             progressDialog.dismiss();
             super.onPostExecute(result);
         }
